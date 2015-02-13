6133336d307f159f5b15526a58c8dd4dac069e70
thegraymonkey <thegraymonkey@gmail.com>
2015-02-13 21:19:11 +0100

    view composer & cleaning 2

---
+++ resources/views/pops/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layout')
+
+@section('content')
+
+@include('common.top_adds')
+
+
+<h4>Most popular Games</h4>
+
+<hr>
+
+@include('common.games')
+
+<div class="row">
+  <div class="col-md-4 col-md-offset-4">
+	{!! $games->render() !!}
+  </div>
+</div>
+
+@include('common.bottom_adds')
+
+@stop
+
+@section('sidebar')
+
+<div style="margin-bottom:20px;">
+@include('common.pic_adds')
+</div>
+<div style="margin-bottom:20px;">
+@include('common.link_adds')
+</div>
+<div style="margin-bottom:20px;">
+@include('common.categories')
+</div>
+<div style="margin-bottom:20px;">
+@include('common.archives')
+</div>
+
+@stop
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+
+@section('content')
+
+@include('common.top_adds')
+
+
+<h4 class="text-primary">Todays Free Games</h4>
+
+
+<hr>
+
+@include('common.games')
+
+<div class="row">
+  <div class="col-md-4 col-md-offset-4">
+	{!! $games->render() !!}
+  </div>
+</div>
+
+@include('common.bottom_adds')
+
+@stop
+
+@section('sidebar')
+
+<div style="margin-bottom:20px;">
+@include('common.pic_adds')
+</div>
+<div style="margin-bottom:20px;">
+@include('common.link_adds')
+</div>
+<div style="margin-bottom:20px;">
+@include('common.categories')
+</div>
+<div style="margin-bottom:20px;">
+@include('common.archives')
+</div>
+
+@stop
